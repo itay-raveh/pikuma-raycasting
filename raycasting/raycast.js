2fe8bc09919929fb00bf50caba595e9b99b56d01
Itay Raveh <itayitay30@gmail.com>
2023-01-23 21:32:29 +0200

    ✨ Render grid

--- raycasting/raycast.js
+++ raycasting/raycast.js
@@ -1,9 +1,12 @@
-const TILE_SIZE = 32;
+const TILE_SIZE = 50;
 const MAP_NUM_ROWS = 11;
 const MAP_NUM_COLS = 15;
 
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
 const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+
+const DARK = "#222";
+const LIGHT = "#fff";
 
 class Map {
   constructor() {
@@ -21,4 +24,30 @@ class Map {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
   }
+
+  draw() {
+    for (const i in this.grid) {
+      for (const j in this.grid[i]) {
+        const x = j * TILE_SIZE;
+        const y = i * TILE_SIZE;
+
+        fill(this.grid[i][j] ? DARK : LIGHT);
+        rect(x, y, TILE_SIZE, TILE_SIZE);
+      }
+    }
+  }
+}
+
+const grid = new Map();
+
+function setup() {
+  createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+  stroke(DARK);
+}
+
+function update() {}
+
+function draw() {
+  update();
+  grid.draw();
 }
