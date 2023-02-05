06a792f80a2d469c6fca60cdd30ecec2ee8c1446
Itay Raveh <83612679+itay-raveh@users.noreply.github.com>
2023-02-05 22:01:11 +0000

    🎨 Make entity globals upper case

--- raycast.js
+++ raycast.js
@@ -1,6 +1,6 @@
 /// <reference path="/home/vscode/.vscode-remote/extensions/samplavigne.p5-vscode-1.2.13/p5types/global.d.ts"/>
 
-// Load `p5` function to globa scope
+// Load `p5` members to globa scope
 new p5();
 
 const TILE_SIZE = 60;
@@ -65,7 +65,7 @@ class Grid {
   hasWallAt(position) {
     return (
       !isInWindow(position) ||
-      !!grid.grid[pixels2index(position.y)][pixels2index(position.x)]
+      !!GRID.grid[pixels2index(position.y)][pixels2index(position.x)]
     );
   }
 
@@ -110,7 +110,7 @@ class Player {
     );
     const position = p5.Vector.add(this.position, move);
 
-    if (!grid.hasWallAt(position)) this.position = position;
+    if (!GRID.hasWallAt(position)) this.position = position;
   }
 
   draw() {
@@ -127,11 +127,9 @@ class Player {
 class Ray {
   constructor(angle) {
     this.angle = angle % (2 * PI);
-
     this.hit = createVector(0, 0);
     this.distance = 0;
     this.isHitVer = false;
-
     this.isFacingDown = this.angle > 0 && this.angle < PI;
     this.isFacingRight = this.angle > 1.5 * PI || this.angle < 0.5 * PI;
   }
@@ -140,10 +138,10 @@ class Ray {
     const intercept = createVector(0, 0);
     const step = createVector(0, 0);
 
-    intercept.y = floor(player.position.y / TILE_SIZE) * TILE_SIZE;
+    intercept.y = floor(PLAYER.position.y / TILE_SIZE) * TILE_SIZE;
     intercept.y += this.isFacingDown ? TILE_SIZE : 0;
     intercept.x =
-      player.position.x + (intercept.y - player.position.y) / tan(this.angle);
+      PLAYER.position.x + (intercept.y - PLAYER.position.y) / tan(this.angle);
     step.y = TILE_SIZE;
     step.y *= this.isFacingDown ? 1 : -1;
     step.x = TILE_SIZE / tan(this.angle);
@@ -156,7 +154,7 @@ class Ray {
     const hor = createVector(0, 0);
     while (isInWindow(next)) {
       if (
-        grid.hasWallAt(
+        GRID.hasWallAt(
           createVector(next.x, next.y - (this.isFacingDown ? 0 : 1))
         )
       ) {
@@ -168,10 +166,10 @@ class Ray {
       next.add(step);
     }
 
-    intercept.x = floor(player.position.x / TILE_SIZE) * TILE_SIZE;
+    intercept.x = floor(PLAYER.position.x / TILE_SIZE) * TILE_SIZE;
     intercept.x += this.isFacingRight ? TILE_SIZE : 0;
     intercept.y =
-      player.position.y + (intercept.x - player.position.x) * tan(this.angle);
+      PLAYER.position.y + (intercept.x - PLAYER.position.x) * tan(this.angle);
     step.x = TILE_SIZE;
     step.x *= this.isFacingRight ? 1 : -1;
     step.y = TILE_SIZE * tan(this.angle);
@@ -184,7 +182,7 @@ class Ray {
     const ver = createVector(0, 0);
     while (isInWindow(next)) {
       if (
-        grid.hasWallAt(
+        GRID.hasWallAt(
           createVector(next.x - (this.isFacingRight ? 0 : 1), next.y)
         )
       ) {
@@ -196,9 +194,9 @@ class Ray {
       next.add(step);
     }
 
-    const horDist = !foundHor ? Number.MAX_VALUE : player.position.dist(hor);
+    const horDist = !foundHor ? Number.MAX_VALUE : PLAYER.position.dist(hor);
 
-    const verDist = !foundVer ? Number.MAX_VALUE : player.position.dist(ver);
+    const verDist = !foundVer ? Number.MAX_VALUE : PLAYER.position.dist(ver);
 
     this.hit = horDist < verDist ? hor : ver;
     this.distance = min(horDist, verDist);
@@ -208,32 +206,31 @@ class Ray {
   draw() {
     stroke("red");
     line(
-      MINIMAP_SCALE * player.position.x,
-      MINIMAP_SCALE * player.position.y,
+      MINIMAP_SCALE * PLAYER.position.x,
+      MINIMAP_SCALE * PLAYER.position.y,
       MINIMAP_SCALE * this.hit.x,
       MINIMAP_SCALE * this.hit.y
     );
   }
 }
 
-const grid = new Grid();
-const player = new Player();
-/** @type {Ray[]} */
-let rays = [];
+const GRID = new Grid();
+const PLAYER = new Player();
+let RAYS = [];
 
 function keyPressed() {
   switch (key) {
     case "w":
-      player.positionOffset.y += 1;
+      PLAYER.positionOffset.y += 1;
       break;
     case "s":
-      player.positionOffset.y -= 1;
+      PLAYER.positionOffset.y -= 1;
       break;
     case "a":
-      player.positionOffset.x += 1;
+      PLAYER.positionOffset.x += 1;
       break;
     case "d":
-      player.positionOffset.x -= 1;
+      PLAYER.positionOffset.x -= 1;
       break;
   }
 }
@@ -241,16 +238,16 @@ function keyPressed() {
 function keyReleased() {
   switch (key) {
     case "w":
-      player.positionOffset.y -= 1;
+      PLAYER.positionOffset.y -= 1;
       break;
     case "s":
-      player.positionOffset.y += 1;
+      PLAYER.positionOffset.y += 1;
       break;
     case "a":
-      player.positionOffset.x -= 1;
+      PLAYER.positionOffset.x -= 1;
       break;
     case "d":
-      player.positionOffset.x += 1;
+      PLAYER.positionOffset.x += 1;
       break;
   }
 }
@@ -262,39 +259,39 @@ function mouseMoved() {
     mouseX > 0 &&
     mouseX < WINDOW_WIDTH
   )
-    player.angle += ((mouseX - pmouseX) * player.rotationSpeed) / windowWidth;
+    PLAYER.angle += ((mouseX - pmouseX) * PLAYER.rotationSpeed) / windowWidth;
 }
 
 function castAllRays() {
-  let angle = player.angle - FOV_HALF;
+  let angle = PLAYER.angle - FOV_HALF;
 
-  rays = [];
+  RAYS = [];
 
   for (let i = 0; i < RAY_COUNT; i++) {
     const ray = new Ray(angle);
     ray.cast();
-    rays.push(ray);
+    RAYS.push(ray);
     angle += FOV / RAY_COUNT;
   }
 }
 
 function drawWalls() {
-  for (const i in rays) {
-    const ray = rays[i];
-    const distance = ray.distance * cos(ray.angle - player.angle);
+  for (const i in RAYS) {
+    const ray = RAYS[i];
+    const distance = ray.distance * cos(ray.angle - PLAYER.angle);
     const wallStripHeight = (TILE_SIZE / distance) * DISTANCE_TO_PROJECTION;
 
     const b = round(
       255 - (distance * 150) / DISTANCE_TO_PROJECTION + (ray.isHitVer ? 50 : 0)
     );
-    fill("black");
+    fill(DARK);
     rect(
       i * RAY_WIDTH,
       WINDOW_HALF_HEIGHT - wallStripHeight / 2 - 1,
       RAY_WIDTH,
       wallStripHeight + 2
     );
-    if (i > 0 && ray.isHitVer !== rays[i - 1].isHitVer) fill("black");
+    if (i > 0 && ray.isHitVer !== RAYS[i - 1].isHitVer) fill(DARK);
     else fill(`rgb(${b},${b},${b})`);
     noStroke();
     rect(
@@ -308,11 +305,10 @@ function drawWalls() {
 
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-  textAlign(CENTER, CENTER);
 }
 
 function update() {
-  player.update();
+  PLAYER.update();
   castAllRays();
 }
 
@@ -326,14 +322,14 @@ function draw() {
 
   drawWalls();
 
-  fill("black");
+  fill(DARK);
   rectMode(CENTER);
   rect(WINDOW_HALF_WIDTH, WINDOW_HALF_HEIGHT, 1, 10);
   rect(WINDOW_HALF_WIDTH, WINDOW_HALF_HEIGHT, 10, 1);
   rectMode(CORNER);
 
   // minimap
-  grid.draw();
-  for (const ray of rays) ray.draw();
-  player.draw();
+  GRID.draw();
+  for (const ray of RAYS) ray.draw();
+  PLAYER.draw();
 }
