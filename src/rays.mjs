285f49cc2736645d909446b6e701cf089b40fc7b
Itay Raveh <83612679+itay-raveh@users.noreply.github.com>
2023-02-11 23:02:50 +0000

    🔥 Remove redundent members from `Ray`

--- src/rays.mjs
+++ src/rays.mjs
@@ -16,25 +16,22 @@ class Ray {
 
   cast(angle) {
     this.angle = normAngle(angle);
-    this.hit = createVector(0, 0);
-    this.distance = 0;
-    this.isHitVer = false;
-    this.isFacingDown = this.angle > 0 && this.angle < PI;
-    this.isFacingRight = this.angle > 1.5 * PI || this.angle < 0.5 * PI;
+    const isFacingDown = this.angle > 0 && this.angle < PI;
+    const isFacingRight = this.angle > PI + HALF_PI || this.angle < HALF_PI;
 
     const intercept = createVector(0, 0);
     const step = createVector(0, 0);
 
     intercept.y = floor(this.player.position.y / TILE_SIZE) * TILE_SIZE;
-    intercept.y += this.isFacingDown ? TILE_SIZE : 0;
+    intercept.y += isFacingDown ? TILE_SIZE : 0;
     intercept.x =
       this.player.position.x +
       (intercept.y - this.player.position.y) / tan(this.angle);
     step.y = TILE_SIZE;
-    step.y *= this.isFacingDown ? 1 : -1;
+    step.y *= isFacingDown ? 1 : -1;
     step.x = TILE_SIZE / tan(this.angle);
-    step.x *= !this.isFacingRight && step.x > 0 ? -1 : 1;
-    step.x *= this.isFacingRight && step.x < 0 ? -1 : 1;
+    step.x *= !isFacingRight && step.x > 0 ? -1 : 1;
+    step.x *= isFacingRight && step.x < 0 ? -1 : 1;
 
     const next = intercept.copy();
 
@@ -43,7 +40,7 @@ class Ray {
     while (isInWindow(next)) {
       if (
         this.grid.hasWallAt(
-          createVector(next.x, next.y - (this.isFacingDown ? 0 : 1))
+          createVector(next.x, next.y - (isFacingDown ? 0 : 1))
         )
       ) {
         foundHor = true;
@@ -55,15 +52,15 @@ class Ray {
     }
 
     intercept.x = floor(this.player.position.x / TILE_SIZE) * TILE_SIZE;
-    intercept.x += this.isFacingRight ? TILE_SIZE : 0;
+    intercept.x += isFacingRight ? TILE_SIZE : 0;
     intercept.y =
       this.player.position.y +
       (intercept.x - this.player.position.x) * tan(this.angle);
     step.x = TILE_SIZE;
-    step.x *= this.isFacingRight ? 1 : -1;
+    step.x *= isFacingRight ? 1 : -1;
     step.y = TILE_SIZE * tan(this.angle);
-    step.y *= !this.isFacingDown && step.y > 0 ? -1 : 1;
-    step.y *= this.isFacingDown && step.y < 0 ? -1 : 1;
+    step.y *= !isFacingDown && step.y > 0 ? -1 : 1;
+    step.y *= isFacingDown && step.y < 0 ? -1 : 1;
 
     next.set(intercept);
 
@@ -72,7 +69,7 @@ class Ray {
     while (isInWindow(next)) {
       if (
         this.grid.hasWallAt(
-          createVector(next.x - (this.isFacingRight ? 0 : 1), next.y)
+          createVector(next.x - (isFacingRight ? 0 : 1), next.y)
         )
       ) {
         foundVer = true;
@@ -91,9 +88,8 @@ class Ray {
       ? Number.MAX_VALUE
       : this.player.position.dist(ver);
 
-    this.hit = horDist < verDist ? hor : ver;
-    this.distance = min(horDist, verDist);
     this.isHitVer = verDist < horDist;
+    this.hit = this.isHitVer ? ver : hor;
   }
 }
 
