84420a3e8389be2eb18dc12b14d43078149091c4
Itay Raveh <83612679+itay-raveh@users.noreply.github.com>
2023-03-08 21:40:17 +0000

    ✨ Add SDL window creation

---
+++ c/src/consts.h
@@ -0,0 +1,7 @@
+// booleans
+#define FALSE 0
+#define TRUE 1
+
+// window dimensions
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
--- c/src/main.c
+++ c/src/main.c
@@ -1,8 +1,60 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main()
-{
-    puts("Hello World!");
+#include "consts.h"
 
-    return 0;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int isGameRunning = FALSE;
+
+int initWindow() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error in SDL initialization\n");
+    return FALSE;
+  }
+
+  window = SDL_CreateWindow("Pikuma Raycasting", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,
+                            SDL_WINDOW_BORDERLESS);
+
+  if (!window) {
+    fprintf(stderr, "Error in SDL window initialization\n");
+    return FALSE;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if (!renderer) {
+    fprintf(stderr, "Error in SDL renderer initialization\n");
+    return FALSE;
+  }
+
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+  return TRUE;
+}
+
+void destroyWindow() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
+int main() {
+  isGameRunning = initWindow();
+
+  // TODO: setup();
+
+  while (isGameRunning) {
+    /*
+    TODO:
+        processInput();
+        update();
+        render();
+    */
+  }
+
+  destroyWindow();
+
+  return 0;
 }
